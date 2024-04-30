cc5ca88ae7ab966482df5df53b1d5771f12cd7fe
asthtls <as_thtls@naver.com>
2024-05-01 06:37:15 +0900

    VGG16_pytorch

--- VGG16_pytorch.py
+++ VGG16_pytorch.py
@@ -1,49 +1,86 @@
-import tensorflow as tf
-from tensorflow.keras import layers
+import torch
+import torch.nn as nn
 
-class VGG16(tf.keras.Model):
-    def __init__(self, num_classes=2):
+class VGG16(nn.Module):
+    def __init__(self, num_classes=1000):
         super(VGG16, self).__init__()
+        # Block 1
+        self.conv1_1 = nn.Conv2d(3, 64, kernel_size=(3, 3), padding=1)
+        self.relu1_1 = nn.ReLU(inplace=True)
+        self.conv1_2 = nn.Conv2d(64, 64, kernel_size=(3, 3), padding=1)
+        self.relu1_2 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-        self.features = tf.keras.Sequential([
-            layers.Conv2D(64, kernel_size=3, padding='same', activation='relu', input_shape=(224, 224, 3)),
-            layers.Conv2D(64, kernel_size=3, padding='same', activation='relu'),
-            layers.MaxPool2D(pool_size=2, strides=2),
+        # Block 2
+        self.conv2_1 = nn.Conv2d(64, 128, kernel_size=(3, 3), padding=1)
+        self.relu2_1 = nn.ReLU(inplace=True)
+        self.conv2_2 = nn.Conv2d(128, 128, kernel_size=(3, 3), padding=1)
+        self.relu2_2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-            layers.Conv2D(128, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(128, kernel_size=3, padding='same', activation='relu'),
-            layers.MaxPool2D(pool_size=2, strides=2),
+        # Block 3
+        self.conv3_1 = nn.Conv2d(128, 256, kernel_size=(3, 3), padding=1)
+        self.relu3_1 = nn.ReLU(inplace=True)
+        self.conv3_2 = nn.Conv2d(256, 256, kernel_size=(3, 3), padding=1)
+        self.relu3_2 = nn.ReLU(inplace=True)
+        self.conv3_3 = nn.Conv2d(256, 256, kernel_size=(3, 3), padding=1)
+        self.relu3_3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-            layers.Conv2D(256, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(256, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(256, kernel_size=3, padding='same', activation='relu'),
-            layers.MaxPool2D(pool_size=2, strides=2),
+        # Block 4
+        self.conv4_1 = nn.Conv2d(256, 512, kernel_size=(3, 3), padding=1)
+        self.relu4_1 = nn.ReLU(inplace=True)
+        self.conv4_2 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1)
+        self.relu4_2 = nn.ReLU(inplace=True)
+        self.conv4_3 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1)
+        self.relu4_3 = nn.ReLU(inplace=True)
+        self.pool4 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.MaxPool2D(pool_size=2, strides=2),
+        # Block 5
+        self.conv5_1 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1)
+        self.relu5_1 = nn.ReLU(inplace=True)
+        self.conv5_2 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1)
+        self.relu5_2 = nn.ReLU(inplace=True)
+        self.conv5_3 = nn.Conv2d(512, 512, kernel_size=(3, 3), padding=1)
+        self.relu5_3 = nn.ReLU(inplace=True)
+        self.pool5 = nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2))
 
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.Conv2D(512, kernel_size=3, padding='same', activation='relu'),
-            layers.MaxPool2D(pool_size=2, strides=2)
-        ])
+        # Classification layers
+        self.fc1 = nn.Linear(512 * 7 * 7, 4096)
+        self.relu6 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(4096, 4096)
+        self.relu7 = nn.ReLU(inplace=True)
+        self.fc3 = nn.Linear(4096, num_classes)
 
-        self.flatten = layers.Flatten()
-        self.classifier = tf.keras.Sequential([
-            layers.Dense(4096, activation='relu'),
-            layers.Dropout(0.5),
-            layers.Dense(4096, activation='relu'),
-            layers.Dropout(0.5),
-            layers.Dense(num_classes, activation='softmax')
-        ])
+    def forward(self, x):
+        x = self.relu1_1(self.conv1_1(x))
+        x = self.relu1_2(self.conv1_2(x))
+        x = self.pool1(x)
 
-    def call(self, inputs):
-        x = self.features(inputs)
-        x = self.flatten(x)
-        x = self.classifier(x)
+        x = self.relu2_1(self.conv2_1(x))
+        x = self.relu2_2(self.conv2_2(x))
+        x = self.pool2(x)
+
+        x = self.relu3_1(self.conv3_1(x))
+        x = self.relu3_2(self.conv3_2(x))
+        x = self.relu3_3(self.conv3_3(x))
+        x = self.pool3(x)
+
+        x = self.relu4_1(self.conv4_1(x))
+        x = self.relu4_2(self.conv4_2(x))
+        x = self.relu4_3(self.conv4_3(x))
+        x = self.pool4(x)
+
+        x = self.relu5_1(self.conv5_1(x))
+        x = self.relu5_2(self.conv5_2(x))
+        x = self.relu5_3(self.conv5_3(x))
+        x = self.pool5(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.relu6(self.fc1(x))
+        x = self.relu7(self.fc2(x))
+        x = self.fc3(x)
+
         return x
 
-# 모델 인스턴스 생성
-model = VGG16()
+# vgg16_model = VGG16()
